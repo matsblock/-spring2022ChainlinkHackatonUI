4b2d3f62047431962f16eead5d8d6b597784cde8
matsblock <matsblock@outlook.com>
2022-05-17 10:27:58 -0300

    ETH PRICE CONSUMER

--- components/Content.js
+++ components/Content.js
@@ -1,8 +1,47 @@
+import { useState, useEffect } from "react"
+import { useMoralis, useWeb3Contract } from "react-moralis"
+import { PriceConsumerV3Abi, PriceConsumerV3Address } from "../constants"
+import { ethers } from "ethers"
+import { Button, Typography } from "web3uikit"
 
 export default function Content() {
+    const { account, isWeb3Enabled } = useMoralis()
+    const { runContractFunction } = useWeb3Contract()
+
+    const [ethPrice, setEthPrice] = useState(0)
+
+    async function updateEthPrice() {
+        console.log("Inside updateEthPrice")
+
+        const _ethPrice = (
+            await getEthPrice({ onError: (error) => console.log(error) })
+        )
+        console.log("ETH PRICE: ", _ethPrice.toNumber())
+
+        setEthPrice(_ethPrice.toNumber()/100000000)
+
+    }
+
+    const { runContractFunction: getEthPrice } = useWeb3Contract({
+        abi: PriceConsumerV3Abi,
+        contractAddress: PriceConsumerV3Address,
+        functionName: "getLatestPrice",
+        params: {},
+    })
+
     return (
         <div>
-            <h3  > Content </h3>
+            <Typography variant="h3"> Eth Price: </Typography>
+            <h4 variant="h4">${ethPrice}</h4>
+
+            <hr></hr>          <Button id="test-button-primary"
+                onClick={updateEthPrice}
+                text="Refresh"
+                theme="primary"
+                type="button"></Button>
+            <h5> PriceConsumerV3Address: {PriceConsumerV3Address} </h5>
+
+
         </div>
 
     )
---
+++ constants/index.js
@@ -0,0 +1,8 @@
+const PriceConsumerV3Address = "0xdA4494619e52574244B38ac72C6033c3c4ed158A"
+
+const PriceConsumerV3Abi = require("./PriceConsumerV3Abi.json")
+
+module.exports = {
+    PriceConsumerV3Abi,
+    PriceConsumerV3Address,
+}
